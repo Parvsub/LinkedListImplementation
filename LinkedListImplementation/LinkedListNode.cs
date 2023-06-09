af7152b6a08011f38499ce8cbdfc2b0542415839
Parvsub <Praveenboda421@gmail.com>
2023-06-09 11:07:48 +0530

    Added : Linked List Operations are performed

---
+++ LinkedListImplementation/LinkedListNode.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace LinkedListImplementation
+{
+    class LinkedListNode<T>
+    {
+        private Node<T> head;
+
+        public class Node<T>
+        {
+            public T Data;
+            public Node<T> Next;
+
+            public Node(T data)
+            {
+                Data = data;
+
+            }
+        }
+        public void AddFirst(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+        }
+
+        public void AddLast(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newNode;
+            }
+        }
+
+        public void AddAfter(T oldData, T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            Node<T> temp = head;
+            while (!temp.Data.Equals(oldData)) 
+            {
+                temp = temp.Next;
+            }
+            newNode.Next = temp.Next;
+            temp.Next = newNode;
+        }
+
+        public void DeleteFirst()
+        {
+            if (head != null)
+            {
+                head = head.Next;
+            }
+        }
+
+        public void DeleteLast()
+        {
+            if (head == null || head.Next == null)
+            {
+                head = null;
+                return;
+            }
+
+            Node<T> temp = head;
+            while (temp.Next.Next != null)
+            {
+                temp = temp.Next;
+            }
+            temp.Next = null;
+        }
+
+        public void DeleteParticular(T data)
+        {
+            if (head == null)
+                return;
+
+            if (head.Data.Equals(data))
+            {
+                head = head.Next;
+                return;
+            }
+            Node<T> temp = head;
+            while (temp.Next != null && !temp.Next.Data.Equals(data))
+            {
+                temp = temp.Next;
+            }
+
+            if (temp.Next == null)
+            {
+                Console.WriteLine("Data not found in the linkedlist");
+                return;
+            }
+
+            temp.Next = temp.Next.Next;
+        }
+        public void Display()
+        {
+            Node<T> temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            else
+            {
+                Console.Write("Linked list: ");
+                while (temp != null)
+                {
+                    Console.Write(temp.Data + "->");
+                    temp = temp.Next;
+                }
+                Console.WriteLine("END");
+            }
+        }
+    }
+}
+
+
--- LinkedListImplementation/Program.cs
+++ LinkedListImplementation/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LinkedListNode<String> linkedList = new LinkedListNode<String>();
+
+            linkedList.AddFirst("Praveen");
+            linkedList.AddFirst("Mohan");
+            linkedList.AddFirst("Sudheer");
+            linkedList.Display();
+
+            linkedList.AddLast("Rohith");
+            linkedList.AddLast("Dhoni");
+            linkedList.Display();
+
+
+            linkedList.AddAfter("Mohan", "Kholi");
+            linkedList.Display();
+
+            linkedList.DeleteFirst();
+            linkedList.Display();
+
+            linkedList.DeleteLast();
+            linkedList.Display();
+
+            linkedList.DeleteParticular("Kholi");
+            linkedList.Display();
         }
     }
 }
